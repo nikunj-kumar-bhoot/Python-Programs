334206858a51ca2d14ed4601a578257322880018
nikunj-kumar-bhoot <nikunjbhoot5@gmail.com>
2026-05-27 16:57:57 +0530

    error rectified

--- quick_sort.py
+++ quick_sort.py
@@ -9,7 +9,7 @@ def partition(nums,low,high):
             j-=1
         if i<j:
             nums[i],nums[j]=nums[j],nums[i]
-        nums[low],nums[j]=nums[j],nums[low]
+    nums[low],nums[j]=nums[j],nums[low]
     return j
 def quick_sort(nums,low,high):
     if low<high:
@@ -17,6 +17,6 @@ def quick_sort(nums,low,high):
         quick_sort(nums,low,p_idx-1)
         quick_sort(nums,p_idx+1,high)
     return nums
-#print(quick_sort([8,3,6,1,2,3,9,5],0,6))
+#print(quick_sort([8,3,6,1,2,3,9,5],0,7))
 #in best and avg case, TC=O(NlogN), SC=O(1)
 #in worst case (when all list elements are equal), TC=O(N^2), SC=O(1)
